3be41d58459c77e52a565c98e18d97d6e207562d
Sebastian Korfmann <sebastian@korfmann.net>
2023-03-15 11:44:21 +0100

    Initial commit

---
+++ test/shorturl.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Shorturl from '../lib/shorturl-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/shorturl-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Shorturl.ShorturlStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
